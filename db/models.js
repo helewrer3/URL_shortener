d5f941cc8ba5f5520baee4e3bcda33375586ac74
helewrer3 <helewrer3@gmail.com>
2021-01-25 21:18:19 +0530

    Debugging

--- db/models.js
+++ db/models.js
@@ -1,12 +1,16 @@
 const sequelize = require('sequelize');
 var db;
-if(process.env.DATABASE_URL)db = new sequelize(process.env.DATABASE_URL)
-else {
-    db = new sequelize({
+if(process.env.DATABASE_URL)db = new sequelize('postgres://tycofzxwcwrqpy:d6f798b8c73c36420f977c43f09049c9fec247c020e8fa1cbb742f2aad7feeb8@ec2-34-235-240-133.compute-1.amazonaws.com:5432/dctlbtsqq5ubv8', {
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+else db = new sequelize({
         dialect: 'sqlite',
         storage: __dirname + '/storage.db'
     });
-}
 
 const mod = db.define('url', {
     id: {
