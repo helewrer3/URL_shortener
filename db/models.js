3b6bd996722560c05a094b71ae1154293d24f3e3
helewrer3 <helewrer3@gmail.com>
2021-01-25 21:18:43 +0530

    Debugging

--- db/models.js
+++ db/models.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 var db;
-if(process.env.DATABASE_URL)db = new sequelize('postgres://tycofzxwcwrqpy:d6f798b8c73c36420f977c43f09049c9fec247c020e8fa1cbb742f2aad7feeb8@ec2-34-235-240-133.compute-1.amazonaws.com:5432/dctlbtsqq5ubv8', {
+if(process.env.DATABASE_URL)db = new sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
         ssl: {
             rejectUnauthorized: false
